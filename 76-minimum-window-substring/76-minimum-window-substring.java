1223d9a5e3e3e68c0fcbccd576a5cc171527fb43
Rehman Murad Ali <rehmanmuradali0@gmail.com>
2022-05-21 00:50:46 +0300

    Time: 124 ms (12.36%), Space: 54.8 MB (8.82%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,39 @@
+class Solution {
+    public static String minWindow(String s, String t) {
+        Map<Character, Integer> occurrence = new HashMap<>();
+        String result = "";
+        for (Character x : t.toCharArray()) {
+            occurrence.put(x, occurrence.getOrDefault(x, 0) + 1);
+        }
+        int left = 0;
+        int right = 0;
+        while (right < s.length() || isValidWindow(occurrence)) {
+
+            if(!isValidWindow(occurrence)) {
+                if(occurrence.containsKey(s.charAt(right))) {
+                    occurrence.put(s.charAt(right), occurrence.get(s.charAt(right)) - 1);
+                }
+                right++;
+                continue;
+            }
+
+            result = (!result.equals("") && (result.length() <= (right - left))) ? result : s.substring(left, right);
+
+            if(occurrence.containsKey(s.charAt(left))) {
+                occurrence.put(s.charAt(left), occurrence.get(s.charAt(left)) + 1);
+            }
+            left++;
+
+        }
+        return result;
+    }
+
+    public static boolean isValidWindow(Map<Character, Integer> map) {
+        for (Map.Entry<Character, Integer> entry : map.entrySet() ) {
+            if (entry.getValue() > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
