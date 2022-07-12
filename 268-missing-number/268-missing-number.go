98b5770b3a2b579143ea8a0dbd3ee6804b2bd462
Rehman Murad Ali <rehmanmuradali0@gmail.com>
2022-07-13 01:57:33 +0300

    Time: 15 ms (92.38%), Space: 6.1 MB (97.38%) - LeetHub

--- 268-missing-number/268-missing-number.go
+++ 268-missing-number/268-missing-number.go
@@ -1,7 +1,8 @@
 func missingNumber(nums []int) int {
-	sum := 0
-	for i := 0; i < len(nums); i++ {
-		sum += nums[i]
+	n := len(nums)
+    sum := (n*(n+1))/2 
+    for i := range nums {
+		sum -= nums[i]
 	}
-	return (len(nums)*(len(nums)+1))/2 - sum
+	return sum
 }
