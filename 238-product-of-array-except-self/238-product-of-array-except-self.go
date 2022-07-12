7e8af21f89d057f1a8b200cf83c237af6183504d
Rehman Murad Ali <rehmanmuradali0@gmail.com>
2022-07-13 01:20:16 +0300

    Time: 35 ms (65.12%), Space: 11 MB (5.17%) - LeetHub

---
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -0,0 +1,21 @@
+func productExceptSelf(nums []int) []int {
+    prefixProduct := make([]int, len(nums)+2)
+	suffixProduct := make([]int, len(nums)+2)
+	result := make([]int, len(nums))
+	prefixProduct[0] = 1
+	suffixProduct[len(nums)+1] = 1
+
+	for i := 1; i < len(nums); i++ {
+		prefixProduct[i] = prefixProduct[i-1] * nums[i-1]
+	}
+
+	for i := len(suffixProduct) - 2; i > 0; i-- {
+		suffixProduct[i] = suffixProduct[i+1] * nums[i-1]
+	}
+
+	for i := 0; i < len(nums); i++ {
+		result[i] = prefixProduct[i] * suffixProduct[i+2]
+
+	}
+	return result
+}
