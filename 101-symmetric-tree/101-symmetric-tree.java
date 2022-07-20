0dd156f87bb2db8cbda9c99f80ef1851fa8a62d8
Rehman Murad Ali <rehmanmuradali0@gmail.com>
2022-07-21 02:45:48 +0300

    Time: 1 ms (63.16%), Space: 42.4 MB (47.00%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -14,34 +14,56 @@
  * }
  */
 class Solution {
+    
     public static boolean isSymmetric(TreeNode root) {
-        List<Integer> left = depthFirstSearch(root.left, false);
-        List<Integer> right = depthFirstSearch(root.right, true);
-        return left.equals(right);
+       //return isSymmetricIterative(root);
+       return isSymmetricRecursive(root);
     }
 
-
-    public static List<Integer> depthFirstSearch(TreeNode root, boolean isLeftOriented) {
-        Stack<TreeNode> stack = new Stack<>();
-        List<Integer> elements = new ArrayList<>();
-        if(root == null) {
-            return new ArrayList<>();
+    private static boolean isSymmetricRecursive(TreeNode root) {
+        if (root == null) {
+            return true;
         }
-        stack.push(root);
-        while(!stack.isEmpty()) {
-            TreeNode current = stack.pop();
-            elements.add(current == null? Integer.MIN_VALUE : current.val);
-            if (current == null){
-                continue;
-            }
-
-            stack.push(isLeftOriented ? current.left: current.right );
-
-            TreeNode secondChild = isLeftOriented? current.right: current.left;
-            stack.push(isLeftOriented ? current.right: current.left );
-
-
-        }
-        return elements;
+        return isSymmetricRecursive(root.left, root.right);
     }
+    private static boolean isSymmetricRecursive(TreeNode left, TreeNode right) {
+
+        if(left == null || right == null) {
+            return left == right;
+        }
+        if(left.val != right.val){
+            return false;
+        }
+        return isSymmetricRecursive(left.left, right.right) && isSymmetricRecursive(left.right, right.left);
+    }
+//     public static boolean isSymmetric(TreeNode root) {
+//         List<Integer> left = depthFirstSearch(root.left, false);
+//         List<Integer> right = depthFirstSearch(root.right, true);
+//         return left.equals(right);
+//     }
+
+
+//     public static List<Integer> depthFirstSearch(TreeNode root, boolean isLeftOriented) {
+//         Stack<TreeNode> stack = new Stack<>();
+//         List<Integer> elements = new ArrayList<>();
+//         if(root == null) {
+//             return new ArrayList<>();
+//         }
+//         stack.push(root);
+//         while(!stack.isEmpty()) {
+//             TreeNode current = stack.pop();
+//             elements.add(current == null? Integer.MIN_VALUE : current.val);
+//             if (current == null){
+//                 continue;
+//             }
+
+//             stack.push(isLeftOriented ? current.left: current.right );
+
+//             TreeNode secondChild = isLeftOriented? current.right: current.left;
+//             stack.push(isLeftOriented ? current.right: current.left );
+
+
+//         }
+//         return elements;
+//     }
 }
