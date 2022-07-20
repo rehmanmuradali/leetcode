97a4b3eef34ec24b9d56dbd2c691e2e1c038fa8b
Rehman Murad Ali <rehmanmuradali0@gmail.com>
2022-07-21 00:49:32 +0300

    Time: 2 ms (23.26%), Space: 44.6 MB (10.40%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -14,21 +14,54 @@
  * }
  */
 class Solution {
-     public static boolean isValidBST(TreeNode root) {
-        return isValidBST(root, null, null);
-    }
-
-
-    public static boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
-        if(root == null){
+    
+    
+    
+    public static boolean isValidBST(TreeNode root) {
+            //return isValidBSTRecursive(root, null, null);
+            return isValidBSTInOrderTraversal(root);
+        }
+    
+        public static boolean isValidBSTInOrderTraversal(TreeNode root) {
+            List<Integer> elements = inOrderTraversal(root, new ArrayList<>());
+            return isSorted(elements);
+        }
+    
+        private static boolean isSorted(List<Integer> elements) {
+            Integer prev = null;
+            for (Integer current :
+                 elements) {
+                if(prev!=null && prev >= current) {
+                    return false;
+                }
+                prev = current;
+            }
             return true;
         }
-        if (min != null && root.val <= min.val){
-            return false;
+    
+        public static List<Integer> inOrderTraversal(TreeNode root, List<Integer> elements){
+            if (root == null) {
+                return  elements;
+            }
+            List<Integer> elemAfterLeft = inOrderTraversal(root.left, elements);
+            elemAfterLeft.add(root.val);
+            return inOrderTraversal(root.right, elemAfterLeft);
         }
-        if (max != null && root.val >= max.val){
-            return false;
-        }
-        return  isValidBST(root.left, min , root ) && isValidBST(root.right, root, max);
-    }
+//      public static boolean isValidBST(TreeNode root) {
+//         return isValidBST(root, null, null);
+//     }
+
+
+//     public static boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+//         if(root == null){
+//             return true;
+//         }
+//         if (min != null && root.val <= min.val){
+//             return false;
+//         }
+//         if (max != null && root.val >= max.val){
+//             return false;
+//         }
+//         return  isValidBST(root.left, min , root ) && isValidBST(root.right, root, max);
+//     }
 }
